3f6932541dcac357bd32df10fd26bc3af0181748
Patrick Sodré <sodre@sodre.co>
2017-04-24 01:17:40 -0400

    MNT: Re-rendered with conda-smithy 2.2.2

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -48,7 +48,7 @@ source run_conda_forge_build_setup
     export CONDA_PY=27
     set +x
     conda build /recipe_root --quiet || exit 1
-    upload_or_check_non_existence /recipe_root nomr --channel=main || exit 1
+    upload_or_check_non_existence /recipe_root nomr --channel=condavisor || exit 1
 touch /feedstock_root/build_artefacts/conda-forge-build-done
 EOF
 
